389d978333a7341b54df3151586b33042761ee71
Aaron Schuster <aaronjschuster@gmail.com>
2023-03-26 10:51:48 -0400

    build out model functions and router endpoints and wire everything

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,23 @@
-// DO YOUR MAGIC
+const Cars = require("./cars-model");
+const router = require("express").Router();
+
+router.get("/", (req, res, next) => {
+    Cars.getAll()
+        .then( cars => res.json(cars))
+        .catch(next);
+});
+
+router.get("/:id", (req, res, next) => {
+    Cars.getById(req.params.id)
+        .then( car => res.json(car))
+        .catch(next);
+});
+
+router.post("/", (req, res, next) => {
+    Cars.create(req.body)
+        .then( newCar => res.status(201).json(newCar))
+        .catch(next);
+});
+
+module.exports = router;
+
--- api/server.js
+++ api/server.js
@@ -1,7 +1,16 @@
 const express = require("express")
 
-const server = express()
+const server = express();
 
-// DO YOUR MAGIC
+server.use(express.json());
+
+const carsRouter = require("./cars/cars-router");
+server.use("/api/cars", carsRouter);
+
+server.use( (err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
 
 module.exports = server
